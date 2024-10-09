05646a56428ac62ea3ec07b569300fad4c0e1492
krouser2 <61723296+krouser2@users.noreply.github.com>
2024-10-08 18:58:28 -0600

    se agregaron dos iconos para el modo oscuro

--- js/app.js
+++ js/app.js
@@ -1,29 +1,44 @@
 document.addEventListener('DOMContentLoaded',function(){
     changeDarkMode();
+
 });
 
 function changeDarkMode(){
-    
-    if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+    let svgDarkMode = document.querySelector('#svg-dark-mode');
+    if (localStorage.getItem('theme') === 'dark' || window.matchMedia('(prefers-color-scheme: dark)').matches) {
         document.body.classList.add('dark-mode');
-    }
-    else{
+        svgDarkMode.src = '/img/darkMode.svg';
+    } else {
         document.body.classList.remove('dark-mode');
+        svgDarkMode.src = '/img/darkModeSun.svg';
     }
 
     window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
         console.log(event.matches)
+        
         const newColorScheme = event.matches ? "dark" : "light";
         if(newColorScheme==='dark'){
             document.body.classList.add('dark-mode');
+            svgDarkMode.src = '/img/darkModeSun.svg';
         }else{
             document.body.classList.remove('dark-mode');
+            svgDarkMode.src = '/img/darkMode.svg';
 
         }
     });
 
     btnDrakMode = document.querySelector('#dark-mode-btn');
-    btnDrakMode.addEventListener('click',function(){
-        document.body.classList.toggle('dark-mode');
+    btnDrakMode.addEventListener('click', function() {
+      
+        let isDarkMode = document.body.classList.contains('dark-mode');
+        
+        if (isDarkMode) {
+            document.body.classList.remove('dark-mode');
+            svgDarkMode.src = '/img/darkModeSun.svg';
+        } else {
+            document.body.classList.add('dark-mode');
+            svgDarkMode.src = '/img/darkMode.svg';
+        }
     });
 }
+
